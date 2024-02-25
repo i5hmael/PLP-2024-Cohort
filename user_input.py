c1a0d4a77aaaf069472a5523c302444a75c28f87
i5hmael <160425849+i5hmael@users.noreply.github.com>
2024-02-25 07:28:11 -0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("what is your name?")
+age=input("how old are you?")
+location=input("where do you stay?")
+print(f"Hi {name}, hope living in {location} has not been stressful on you as you turn {age}")
